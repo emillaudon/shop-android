0380add25ced6c37fdda6ceb10a8212997cba4ae
emillaudon <emil.laudn@gmail.com>
2026-02-08 17:45:10 +0100

    feat(products): add building image url to productmapper

--- app/src/main/java/com/example/shopmobile/feature/products/data/mapper/ProductMapper.kt
+++ app/src/main/java/com/example/shopmobile/feature/products/data/mapper/ProductMapper.kt
@@ -3,12 +3,13 @@ package com.example.shopmobile.feature.products.data.mapper
 import com.example.shopmobile.feature.products.data.remote.ProductDto
 import com.example.shopmobile.feature.products.domain.model.Product
 
+private const val BASE_URL = "http://10.0.2.2:8080"
 fun ProductDto.toDomain(): Product {
     return Product(
         id = id,
         name = name,
         price = price,
         stock = stock,
-        imageUrl = imageUrl ?: ""
+        imageUrl = imageUrl ?.let { BASE_URL + it } ?: ""
     )
 }
