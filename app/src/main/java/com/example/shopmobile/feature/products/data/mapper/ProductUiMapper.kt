66bd337746ef46c2a22360504c69052d552ed607
emillaudon <emil.laudn@gmail.com>
2026-02-08 17:02:05 +0100

    feat(products): add productuimapper

---
+++ app/src/main/java/com/example/shopmobile/feature/products/data/mapper/ProductUiMapper.kt
@@ -0,0 +1,12 @@
+package com.example.shopmobile.feature.products.data.mapper
+
+import com.example.shopmobile.feature.products.domain.model.Product
+import com.example.shopmobile.feature.products.ui.ProductUi
+
+fun Product.toUi(): ProductUi {
+    return ProductUi(
+        id = id.toString(),
+        name = name,
+        priceText = price.toString()
+    )
+}
