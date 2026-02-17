b74012ab0279b047e5ae850eec0a75cff02ac7e2
emillaudon <emil.laudn@gmail.com>
2026-02-17 20:39:24 +0100

    feat(cart-ui): add initial cartproductcard

---
+++ app/src/main/java/com/example/shopmobile/feature/cart/components/CartProductCard.kt
@@ -0,0 +1,17 @@
+package com.example.shopmobile.feature.cart.components
+
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.shopmobile.feature.products.ui.ProductUi
+
+@Composable
+public fun CartProductCard(
+    product: ProductUi
+) {
+    Card(
+        modifier = Modifier,
+    ) {
+
+    }
+}
