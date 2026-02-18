dae4215dd09c9eeec5850fac18bc5023ec51f82a
emillaudon <emil.laudn@gmail.com>
2026-02-18 19:25:41 +0100

    feat(cart-ui): add column + row in cartproductcard + placeholder for image and info

--- app/src/main/java/com/example/shopmobile/feature/cart/components/CartProductCard.kt
+++ app/src/main/java/com/example/shopmobile/feature/cart/components/CartProductCard.kt
@@ -1,11 +1,20 @@
 package com.example.shopmobile.feature.cart.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -16,10 +25,38 @@ public fun CartProductCard(
     product: ProductUi
 ) {
     Card(
-        modifier = Modifier.background(Color.Black)
-            .height(100.dp)
+        modifier = Modifier
+            .height(140.dp)
             .fillMaxWidth()
-    ) {
 
+    ) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(modifier = Modifier
+                .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+
+            ) {
+                Text("Name")
+                Text("$99")
+            }
+
+            Row(modifier = Modifier
+                .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.Bottom) {
+                Box(
+                    modifier = Modifier
+                        .size(80.dp)
+                        .background(Color.Blue)
+                )
+                Button(onClick = {}) { Text("-") }
+                Text("1")
+                Button(onClick = {}) { Text("+") }
+            }
+        }
     }
 }
